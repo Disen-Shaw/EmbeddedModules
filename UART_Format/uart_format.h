3f7d83a25e0edfa8b4759115f47af71917fb355a
Disen Shaw <shaodisheng1314@gmail.com>
2023-03-20 19:09:54 +0800

    style(UART): Code Re-Format

--- UART_Format/uart_format.h
+++ UART_Format/uart_format.h
@@ -5,38 +5,44 @@
 #include <stdio.h>
 
 // [] format:  [___0000___]
-// LOG
+// log
 
-#define LOG_PRINTF_ENABLE (1U)
+#define LOG_PRINTF_ENABLE (1u)
 
 #if (LOG_PRINTF_ENABLE)
 
-#define LOG_ADVANCED(status, format, ...) \
-    do {                                  \
-        printf("[   %s   ]"               \
-               "\t\t" format,             \
-            status, ##__VA_ARGS__);       \
-        printf("\r\n");                   \
-    } while (0)
+#define LOG_ADVANCED(status, format, ...)                                      \
+  do {                                                                         \
+    printf("[   %s   ]"                                                        \
+           "\t\t" format,                                                      \
+           status, ##__va_args__);                                             \
+    printf("\r\n");                                                            \
+  } while (0)
 
-#define LOG_NORMAL(format, ...)                 \
-    do {                                        \
-        printf("\t\t\t" format, ##__VA_ARGS__); \
-        printf("\r\n");                         \
-    } while (0)
+#define LOG_NORMAL(format, ...)                                                \
+  do {                                                                         \
+    printf("\t\t\t" format, ##__va_args__);                                    \
+    printf("\r\n");                                                            \
+  } while (0)
 
-#define LOG_LINE_LIGHT                                                                           \
-    do {                                                                                         \
-        printf("[   ****   ]\t\t---------------------------------------------------------\r\n"); \
-    } while (0);
-#define LOG_LINE_HEAVY                                                                           \
-    do {                                                                                         \
-        printf("[   ****   ]\t\t=========================================================\r\n"); \
-    } while (0);
-#define LOG_LINE_HEAVIEST                                                                        \
-    do {                                                                                         \
-        printf("[   ****   ]\t\t#########################################################\r\n"); \
-    } while (0);
+#define LOG_LINE_LIGHT                                                         \
+  do {                                                                         \
+    printf(                                                                    \
+        "[   ****   "                                                          \
+        "]\t\t---------------------------------------------------------\r\n"); \
+  } while (0);
+#define LOG_LINE_HEAVY                                                         \
+  do {                                                                         \
+    printf(                                                                    \
+        "[   ****   "                                                          \
+        "]\t\t=========================================================\r\n"); \
+  } while (0);
+#define LOG_LINE_HEAVIEST                                                      \
+  do {                                                                         \
+    printf(                                                                    \
+        "[   ****   "                                                          \
+        "]\t\t#########################################################\r\n"); \
+  } while (0);
 
 #else
 
@@ -48,7 +54,6 @@
 #define LOG_LINE_HEAVY
 #define LOG_LINE_HEAVIEST
 
-#endif /* LOG_PRINTF_ENABLE */
+#endif /* log_printf_enable */
 
-
-#endif /* __BSP_USART_H__ */
+#endif /* __bsp_usart_h__ */
