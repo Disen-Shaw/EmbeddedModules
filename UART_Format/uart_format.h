2f57839dd6425f2f0c39850da1ce5514d80045f9
Disen Shaw <shaodisheng1314@gmail.com>
2023-03-20 19:01:12 +0800

    feat(UART): Add the judgment of whether to use this function

--- UART_Format/uart_format.h
+++ UART_Format/uart_format.h
@@ -1,26 +1,54 @@
 
-#ifndef __UART_FORMAT_H__
-#define __UART_FORMAT_H__
-
+#ifndef __BSP_USART_H__
+#define __BSP_USART_H__
 
 #include <stdio.h>
 
-#define 	LOG_ADVANCED(status ,format, ...) 	do { printf("[   %s   ]" \
-											"\t\t" \
-											format,status,  ##__VA_ARGS__); \
-											printf("\r\n"); \
-											} while(0)
+// [] format:  [___0000___]
+// LOG
 
-#define 	LOG_NORMAL(format,...)		do { printf("\t\t\t" \
-											format,##__VA_ARGS__); \
-											printf("\r\n"); \
-											} while(0)
+#define LOG_PRINTF_ENABLE (1U)
 
-											
-#define 	LOG_LINE_LIGHT				do { printf("[   ****   ]\t\t---------------------------------------------------------\r\n"); } while(0);
-#define 	LOG_LINE_HEAVY				do { printf("[   ****   ]\t\t=========================================================\r\n"); } while(0);
-#define 	LOG_LINE_HEAVIEST			do { printf("[   ****   ]\t\t#########################################################\r\n"); } while(0);
+#if (LOG_PRINTF_ENABLE)
+
+#define LOG_ADVANCED(status, format, ...) \
+    do {                                  \
+        printf("[   %s   ]"               \
+               "\t\t" format,             \
+            status, ##__VA_ARGS__);       \
+        printf("\r\n");                   \
+    } while (0)
+
+#define LOG_NORMAL(format, ...)                 \
+    do {                                        \
+        printf("\t\t\t" format, ##__VA_ARGS__); \
+        printf("\r\n");                         \
+    } while (0)
+
+#define LOG_LINE_LIGHT                                                                           \
+    do {                                                                                         \
+        printf("[   ****   ]\t\t---------------------------------------------------------\r\n"); \
+    } while (0);
+#define LOG_LINE_HEAVY                                                                           \
+    do {                                                                                         \
+        printf("[   ****   ]\t\t=========================================================\r\n"); \
+    } while (0);
+#define LOG_LINE_HEAVIEST                                                                        \
+    do {                                                                                         \
+        printf("[   ****   ]\t\t#########################################################\r\n"); \
+    } while (0);
+
+#else
+
+#define LOG_ADVANCED(status, format, ...)
+
+#define LOG_NORMAL(format, ...)
+
+#define LOG_LINE_LIGHT
+#define LOG_LINE_HEAVY
+#define LOG_LINE_HEAVIEST
+
+#endif /* LOG_PRINTF_ENABLE */
 
 
-#endif /* __UART_FORMAT_H__ */
-
+#endif /* __BSP_USART_H__ */
