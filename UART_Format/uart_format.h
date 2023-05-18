8887ca07331bc8caff06960e550e5c02cb1a5c36
Disen Shaw <shaodisheng1314@gmail.com>
2023-05-18 09:46:36 +0800

    feat(USART_FORMAT): Separate `LOG` information from `DEBUG` information

--- UART_Format/uart_format.h
+++ UART_Format/uart_format.h
@@ -7,11 +7,57 @@
 // [] format:  [___0000___]
 // log
 
-#define LOG_PRINTF_ENABLE (1u)
+#define LOG_DEBUG_ENABLE (1U)
 
-#if (LOG_PRINTF_ENABLE)
+#if (LOG_DEBUG_ENABLE)
 
-#define LOG_ADVANCED(status, format, ...)                                      \
+#define LOG_DEBUG_INFO(status, format, ...)                                    \
+  do {                                                                         \
+    printf("[   %s   ]"                                                        \
+           "\t\t" format,                                                      \
+           status, ##__VA_ARGS__);                                             \
+    printf("\r\n");                                                            \
+  } while (0)
+
+#define LOG_DEBUG_NORMAL(format, ...)                                          \
+  do {                                                                         \
+    printf("\t\t\t" format, ##__VA_ARGS__);                                    \
+    printf("\r\n");                                                            \
+  } while (0)
+
+#define LOG_DEBUG_LINE_LIGHT                                                   \
+  do {                                                                         \
+    printf(                                                                    \
+        "[   ****   "                                                          \
+        "]\t\t---------------------------------------------------------\r\n"); \
+  } while (0);
+
+#define LOG_DEBUG_LINE_HEAVY                                                   \
+  do {                                                                         \
+    printf(                                                                    \
+        "[   ****   "                                                          \
+        "]\t\t=========================================================\r\n"); \
+  } while (0);
+
+#define LOG_DEBUG_LINE_HEAVIEST                                                \
+  do {                                                                         \
+    printf(                                                                    \
+        "[   ****   "                                                          \
+        "]\t\t#########################################################\r\n"); \
+  } while (0);
+
+#else
+
+#define LOG_DEBUG_INFO(status, format, ...)
+#define LOG_DEBUG_NORMAL(format, ...)
+
+#define LOG_DEBULOG_DEBUG_LINE_LIGHT
+#define LOG_DEBUG_LINE_HEAVY
+#define LOG_DEBUG_LINE_HEAVIEST
+
+#endif /* LOG_DEBUG_ENABLE */
+
+#define LOG_INFO(status, format, ...)                                          \
   do {                                                                         \
     printf("[   %s   ]"                                                        \
            "\t\t" format,                                                      \
@@ -31,29 +77,19 @@
         "[   ****   "                                                          \
         "]\t\t---------------------------------------------------------\r\n"); \
   } while (0);
+
 #define LOG_LINE_HEAVY                                                         \
   do {                                                                         \
     printf(                                                                    \
         "[   ****   "                                                          \
         "]\t\t=========================================================\r\n"); \
   } while (0);
+
 #define LOG_LINE_HEAVIEST                                                      \
   do {                                                                         \
     printf(                                                                    \
         "[   ****   "                                                          \
         "]\t\t#########################################################\r\n"); \
   } while (0);
-
-#else
-
-#define LOG_ADVANCED(status, format, ...)
-
-#define LOG_NORMAL(format, ...)
-
-#define LOG_LINE_LIGHT
-#define LOG_LINE_HEAVY
-#define LOG_LINE_HEAVIEST
-
-#endif /* log_printf_enable */
 
 #endif /* __bsp_usart_h__ */
